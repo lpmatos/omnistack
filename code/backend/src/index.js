d345b73d422b3f36a67e7701e854a0171f7e8188
lpmatos <58797390+lpmatos@users.noreply.github.com>
2020-03-27 02:00:01 -0300

    :new: Finish Backend with pagination and Headers total incidents

--- code/backend/src/index.js
+++ code/backend/src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const port = 3333;
 const routes = require("./routes/routes");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- code/backend/src/routes/routes.js
+++ code/backend/src/routes/routes.js
@@ -5,12 +5,20 @@ const express = require("express");
 // Getting Controllers
 const OngController = require("../controllers/OngController");
 const IncidentController = require("../controllers/IncidentController");
+const ProfileController = require("../controllers/ProfileController");
+const SessionController = require("../controllers/SessionController");
 
 const routes = express.Router();
+
+// Routes - Session
+routes.post("/sessions", SessionController.create);
 
 // Routes - Ongs
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
+
+// Routes - Profile
+routes.get("/profile", ProfileController.index);
 
 // Routes - Incidents
 routes.get("/incidents", IncidentController.index);
