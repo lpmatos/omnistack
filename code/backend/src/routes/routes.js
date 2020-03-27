1a940030dccc6c3f58aa5f541e3a245648a632c5
lpmatos <58797390+lpmatos@users.noreply.github.com>
2020-03-27 02:09:07 -0300

    :new: Adding healthcheck route in backend and mapping HEALTHCHECK in Dockerfile

--- code/backend/src/routes/routes.js
+++ code/backend/src/routes/routes.js
@@ -25,4 +25,10 @@ routes.get("/incidents", IncidentController.index);
 routes.post("/incidents", IncidentController.create);
 routes.delete("/incidents/:id", IncidentController.delete);
 
+// Routes - Healthcheck
+routes.get("/healthcheck", (request, response) => {
+    console.log("Health Check Request");
+    response.status(200).end();
+});
+
 module.exports = routes;
